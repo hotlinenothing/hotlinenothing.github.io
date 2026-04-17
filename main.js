33a419f7ddfa32e19d99af3770fd678b9f285554
hotlinenothing <hotlinenothing@gmail.com>
2026-04-17 14:52:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ const center = box.getCenter(new THREE.Vector3());
 camera.position.set(0, 0, size * 1.5);
 camera.lookAt(center);
 
-  model.scale.set(0.07, 0.07, 0.07);
+  model.scale.set(0.2, 0.2, 0.2);
 
   // pivot setup
   pivot = new THREE.Group();
