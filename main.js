c362ac3e4fbf2cad38121b1069d636a788c3ede6
hotlinenothing <hotlinenothing@gmail.com>
2026-04-20 17:24:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 2.5;
+camera.position.z = 2;
 
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
